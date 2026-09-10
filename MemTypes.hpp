42f3249626cbf2fad6441f5643e9390883bc7d3c
gmbjo <gmbjoao@gmail.com>
2026-09-10 20:08:46 +0100

    Creates the TYPE1Gb time withoverflow detection

--- MemTypes.hpp
+++ MemTypes.hpp
@@ -71,18 +71,162 @@ inline void free_mem(void* ptr) {
 
 
 class TYPE16b {
-private:
+public:
 	friend TYPE16b operator+(const TYPE16b& first, const TYPE16b& second) {
 		TYPE16b temp;
 		temp.high = first.high + second.high;
 		temp.low = first.low + second.low;
 		if (temp.low < first.low) {
 			temp.high += 1;
-			return temp;
 		}
+		return temp;
 	}
-public:
+	friend TYPE16b operator-(const TYPE16b& first, const TYPE16b& second) {
+		TYPE16b temp;
+		temp.high = first.high - second.high;
+		temp.low = first.low - second.low;
+		if (temp.low > first.low) {
+			temp.high -= 1;
+		}
+		return temp;
+	}
+	friend bool operator<(const TYPE16b& first, const TYPE16b& second) {
+		if (first.high == second.high && first.low == second.low) return false;
+		if (first.high < second.high) return true;
+		if (first.high > second.high) return false;
+		if (first.low < second.low) return true;
+		if (first.low > second.low) return false;
+		return false;
+	}
+	friend bool operator>(const TYPE16b& first, const TYPE16b& second) {
+		if (first.high == second.high && first.low == second.low) return false;
+		if (first.high > second.high) return true;
+		if (first.high < second.high) return false;
+		if (first.low > second.low) return true;
+		if (first.low < second.low) return false;
+		return false;
+	}
+	friend bool operator<=(const TYPE16b& first, const TYPE16b& second) {
+		if (first.high == second.high && first.low == second.low) return true;
+		if (first.high < second.high) return true;
+		if (first.high > second.high) return false;
+		if (first.low < second.low) return true;
+		if (first.low > second.low) return false;
+		return false;
+	}
+	friend bool operator>=(const TYPE16b& first, const TYPE16b& second) {
+		if (first.high == second.high && first.low == second.low) return true;
+		if (first.high > second.high) return true;
+		if (first.high < second.high) return false;
+		if (first.low > second.low) return true;
+		if (first.low < second.low) return false;
+		return false;
+	}
+
 	TYPE8b high = 0;
 	TYPE8b low = 0;
 	
+	void operator+=(const TYPE16b& first){
+		*this = *this + first;
+	}
+	TYPE16b(TYPE8b num) {
+		this->low = num;
+	}
+	TYPE16b(){}
+	void operator+=(const TYPE8b& first) {
+		TYPE8b before = this->low;
+		this->low += first;
+		if (this->low < before) {
+			this->high += 1;
+		}
+	}
+	void operator-=(const TYPE8b& first) {
+		TYPE8b before = this->low;
+		this->low -= first;
+		if (this->low > before) {
+			this->high -= 1;
+		}
+	}
+
+	
+};
+
+class TYPE1Gb {
+private:
+	TYPE16b* boxes = nullptr;
+	TYPE8b n_boxes = (1024ULL*1024ULL*1024ULL) / 16ULL;
+public:
+	TYPE1Gb(const TYPE1Gb& other) {
+		this->boxes = (TYPE16b*)alloc_mem(1024ULL * 1024ULL * 1024ULL);
+		this->n_boxes = other.n_boxes;
+
+		if (this->boxes != nullptr && other.boxes != nullptr) {
+			for (TYPE8b a = 0; a < n_boxes; ++a) {
+				this->boxes[a] = other.boxes[a];
+			}
+		}
+	}
+	TYPE1Gb& operator=(const TYPE1Gb& other) {
+		if (this != &other) {
+			if (this->boxes != nullptr && other.boxes != nullptr) {
+				for (TYPE8b a = 0; a < n_boxes; ++a) {
+					this->boxes[a] = other.boxes[a];
+				}
+			}
+		}
+		return *this;
+	}
+	friend TYPE1Gb operator+(const TYPE1Gb& first, const TYPE1Gb& second) {
+		TYPE1Gb temp;
+		TYPE8b plus_one = 0; //Like math (contas em pe)
+		for (TYPE8b a = 0; a < first.n_boxes; ++a) {
+			TYPE8b before = first.boxes[a].high; 
+			TYPE16b transporter;             // Need to transport thew plus_one
+			transporter.low = plus_one;
+			transporter.high = 0;
+			temp.boxes[a] = first.boxes[a] + second.boxes[a] + transporter; //Adds
+			if (temp.boxes[a].high < before || (plus_one == 1 && temp.boxes[a].high == before)) plus_one = 1; //Check if it overflows
+			else plus_one = 0;
+		}
+		return temp;
+	}
+	TYPE1Gb() {
+		boxes = (TYPE16b*) alloc_mem(1024ULL * 1024ULL * 1024ULL);
+		for (TYPE8b a = 0; a < n_boxes; ++a) {
+			boxes[a].low = 0;
+			boxes[a].high = 0;
+		}
+	}
+	~TYPE1Gb() {
+		free_mem(boxes);
+	}
+	void operator+=(const TYPE1Gb& a) {
+		*this = *this + a;
+	}
+	TYPE1Gb(const char* num_cstr) {
+		boxes = (TYPE16b*)alloc_mem(1024ULL * 1024ULL * 1024ULL);
+		for (TYPE8b a = 0; a < n_boxes; ++a) {
+			boxes[a].low = 0;
+			boxes[a].high = 0;
+		}
+		int i = 0;
+		TYPE1Gb temp_num;
+		while (num_cstr[i] != '\0') {
+			if (num_cstr[i] >= '0' && num_cstr[i] <= '9') {
+				int num = num_cstr[i] - '0';
+				//Multiplies by 10
+				TYPE1Gb x2 = *this;
+				x2 += *this;
+				TYPE1Gb x8 = x2;
+				x8 += x8;
+				x8 += x8;
+				*this = x8 + x2;
+				temp_num.boxes[0].low = num;
+				*this += temp_num;
+			}
+			++i;
+		}
+	}
+
+
 };
--- main.cpp
+++ main.cpp
@@ -3,16 +3,6 @@
 using namespace std;
 
 int main() {
-	int* ptr = (int*) alloc_mem(sizeof(int) * 268435456);
-	for (int i = 0; i < 268435456; i++) {
-		ptr[i] = i;
-	}
-	system("pause");
-	for (int i = 0; i < 268435456; i++) {
-		cout << ptr[i] << "\n";
-	}
-	system("pause");
-	free_mem(ptr);
-	system("pause");
+	TYPE1Gb a = "667376782678687266736763423432";
 	return 0;
 }
